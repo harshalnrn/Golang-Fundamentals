61f839b77803b24d520ff03b065e83ecebc29de9
Harshal <harshalnrn>
2022-01-13 17:08:00 +0530

    added function type and its usage

--- customTypes.go
+++ customTypes.go
@@ -6,7 +6,7 @@ import(
 
 const num1,num2,num3=1,2,3    //initializing multiple constants in single line // datatypes not required here?
 var data1,data2,data3 string        //declaring variables in single line of same datatype
-//declaring custom types using interface and structs
+//declaring custom types using interface , structs and function
 type idRegisteration interface{
 registerWithIdProof()
 }
@@ -33,6 +33,8 @@ type LicenseBasedRegisteration struct{
 
 
 
+type Greeting func (name string)string         // function type declaration
+
 
 //-----------------------
 
@@ -40,9 +42,13 @@ func main(){
 	// initialise struct variables and call implemented methods of interface of respective struct
 	a:=AadharBasedRegisteration{aadharId: "1212121dadada"}
 	l:=LicenseBasedRegisteration{licenseId:"12345"}
+	greet:=func (name string) string {          //assign declared function type body, to a variable //note like javascript, no function name when assigning to variable
+	return "Hello "+name
+	}
 	a.registerWithIdProof()
 	l.registerWithIdProof()
-	//createAndPopulateMaps()
+	say(greet)
+
 }
 
 //Go is very similar to C due to presence of pointers, static typing and many other things. But Go is a modern language and so it has many new features baked in it.One notable feature is receiver functions.
@@ -56,4 +62,11 @@ func (a AadharBasedRegisteration) registerWithIdProof(){
 
 func (l LicenseBasedRegisteration) registerWithIdProof(){
 	fmt.Println("user registered with licence with licence id:", l.licenseId)
+}
+
+//method with greeting type parameter, which is a function
+func say(greet Greeting){
+
+	//this is helpful when functions cannot be directly from within due to scope protection
+	fmt.Println(greet("harshal"))
 }
